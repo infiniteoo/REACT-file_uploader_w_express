c997c4cff7cb664b4005c919f76d3636b1d759c9
Troy Dorman <troydorman@gmail.com>
2021-07-21 10:24:17 -0600

    wrote front end axios upload logic to successfully upload a file through the backend to the public/uploads dir.  also had to change the proxy in the package.json

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,17 +1,42 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const FileUpload = () => {
   const [file, setFile] = useState("");
   const [filename, setFilename] = useState("Choose File");
+  const [uploadedFile, setUploadedFile] = useState({});
 
   const onChange = (e) => {
     setFile(e.target.files[0]);
     setFilename(e.target.files[0].name);
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const res = await axios.post("/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      const { fileName, filePath } = res.data;
+      setUploadedFile({ fileName, filePath });
+    } catch (err) {
+      if (err.response.status === 500) {
+        console.log("There was a problem with the server.");
+      } else {
+        console.log(err.response.data.msg);
+      }
+    }
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="custom-file mb-4">
           <input
             type="file"
