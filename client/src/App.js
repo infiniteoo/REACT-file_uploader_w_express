922600c271c2a2cd3bc7a0313b01c07cba4d2ff1
Troy Dorman <troydorman@gmail.com>
2021-07-21 07:25:58 -0600

    created our header icon and title

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import "./App.css";
 
-const App = () => <div className="App">APP</div>;
+const App = () => (
+  <div className="container mt-4">
+    <h4 className="display-4 text-center mb-4">
+      <i className="fab fa-react"></i>React File Upload
+    </h4>
+  </div>
+);
 
 export default App;
