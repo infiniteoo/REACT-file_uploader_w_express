28570da1905710d7797ec50941bbde6b342c4915
Troy Dorman <troydorman@gmail.com>
2021-07-21 07:08:02 -0600

    created post route in our express app to handle the file upload request from the client side

--- server.js
+++ server.js
@@ -5,4 +5,22 @@ const app = express();
 
 app.use(fileUpload());
 
+// upload endpoint route
+app.post("/upload", (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ msg: "no file was uploaded" });
+  }
+
+  const file = req.files.file;
+
+  file.mv(`${__dirname}/client/public/uploads/${file.name}}`, (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
+    }
+  });
+
+  res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+});
+
 app.listen(5000, () => console.log("server started..."));
