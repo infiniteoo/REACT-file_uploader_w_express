d258e9d321c34782fca71420f59ed93057598109
Troy Dorman <troydorman@gmail.com>
2021-07-21 10:38:02 -0600

    made it so bootstrap alert appears upon an error or successful upload.  like with the file chooser form, the alerts are not showing correctly. must be a conflict somewhere

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Message from "./Message";
 
 const FileUpload = () => {
   const [file, setFile] = useState("");
   const [filename, setFilename] = useState("Choose File");
   const [uploadedFile, setUploadedFile] = useState({});
+  const [message, setMessage] = useState("");
 
   const onChange = (e) => {
     setFile(e.target.files[0]);
@@ -25,17 +27,19 @@ const FileUpload = () => {
 
       const { fileName, filePath } = res.data;
       setUploadedFile({ fileName, filePath });
+      setMessage("File Uploaded");
     } catch (err) {
       if (err.response.status === 500) {
-        console.log("There was a problem with the server.");
+        setMessage("There was a problem with the server.");
       } else {
-        console.log(err.response.data.msg);
+        setMessage(err.response.data.msg);
       }
     }
   };
 
   return (
     <>
+      {message ? <Message msg={message} /> : null}
       <form onSubmit={onSubmit}>
         <div className="custom-file mb-4">
           <input
@@ -55,14 +59,14 @@ const FileUpload = () => {
         />
       </form>
 
-      {uploadedFile ? 
+      {uploadedFile ? (
         <div className="row mt-5">
           <div className="col-md-6 m-auto">
             <h3 className="text-center">{uploadedFile.fileName}</h3>
             <img style={{ width: "100%" }} src={uploadedFile.filePath} alt="" />
           </div>
         </div>
-      : null}
+      ) : null}
     </>
   );
 };
